c87a4f81a47e523bab50532fa22c26c0d34e16f4
colinl11 <colinl@nycstudents.net>
2025-04-05 21:45:06 -0400

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+
+export default function App() {
+
+
+  
+  return (
+    
+    <main>
+      
+       <h1 style={{color: "greenyellow"}}>This is  my {1 + 0}st time doing a all about me with JSX </h1>
+
+      <h2> My Name is Colin Liang a senior in Bayside High School soon to graduate from Bayside High School </h2>
+      
+        <ul>
+          <li>I like bike riding outside in a sunny blue sky with perfect weather</li>
+          <li>I hate boredom</li>
+          <li>I like reading, learning, doing video games and hanging out with friends</li>
+        </ul>
+
+      <h2 style={{color: "#808183"}}>⬇ Other info I did in the past time ⬇</h2>
+        
+<p> This is me and my partner's 12th grade escaping reality project: </p>
+<a href= {'https://bennybaik.github.io/proj/'}> clickable link</a>
+      
+<p>This is my resume in a google doc: </p>
+<a href= {'https://docs.google.com/document/d/1shyJ4qtKw7ZMxnJDzxa8gMZ6B_WyIlnYpV_nBvvfVI4/edit?tab=t.0'}> clickable link</a>
+      <div>
+        <p>
+        </p>
+        <img src = {'https://upload.wikimedia.org/wikipedia/commons/3/38/School-main-4K.jpg'}/>
+      </div>
+    </main>
+  )
+}
